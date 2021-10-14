d7c414dff47985a498a198ecc8ef235ef3d0d300
Seeun <duck_sim@naver.com>
2021-10-14 14:32:23 +0900

    완성본 수정

--- calendarpj/mycalendar/static/javascript/index.js
+++ calendarpj/mycalendar/static/javascript/index.js
@@ -1,11 +1,14 @@
+// Date 객체 생성
 let date = new Date();
 
-const renderCalender = () => {
+const renderCalendar = () => {
   const viewYear = date.getFullYear();
   const viewMonth = date.getMonth();
 
+  // year-month 채우기
   document.querySelector('.year-month').textContent = `${viewYear}년 ${viewMonth + 1}월`;
 
+  // 지난 달 마지막 Date, 이번 달 마지막 Date
   const prevLast = new Date(viewYear, viewMonth, 0);
   const thisLast = new Date(viewYear, viewMonth + 1, 0);
 
@@ -15,33 +18,41 @@ const renderCalender = () => {
   const TLDate = thisLast.getDate();
   const TLDay = thisLast.getDay();
 
+  // Dates 기본 배열들
   const prevDates = [];
   const thisDates = [...Array(TLDate + 1).keys()].slice(1);
   const nextDates = [];
 
+  // prevDates 계산
   if (PLDay !== 6) {
     for (let i = 0; i < PLDay + 1; i++) {
       prevDates.unshift(PLDate - i);
     }
   }
 
+  // nextDates 계산
   for (let i = 1; i < 7 - TLDay; i++) {
-    nextDates.push(i);
+    nextDates.push(i)
   }
 
+  // Dates 합치기
   const dates = prevDates.concat(thisDates, nextDates);
+
+  // Dates 정리
   const firstDateIndex = dates.indexOf(1);
   const lastDateIndex = dates.lastIndexOf(TLDate);
 
   dates.forEach((date, i) => {
     const condition = i >= firstDateIndex && i < lastDateIndex + 1
-                      ? 'this'
-                      : 'other';
-    dates[i] = `<div class="date"><span class=${condition}>${date}</span></div>`;
-  });
+      ? 'this'
+      : 'other';
+    dates[i] = `<div class="date"><span class="${condition}">${date}</span></div>`;
+  })
 
+  // Dates 그리기
   document.querySelector('.dates').innerHTML = dates.join('');
 
+  // 오늘 날짜 그리기
   const today = new Date();
   if (viewMonth === today.getMonth() && viewYear === today.getFullYear()) {
     for (let date of document.querySelectorAll('.this')) {
@@ -51,21 +62,22 @@ const renderCalender = () => {
       }
     }
   }
-};
+}
 
-renderCalender();
+renderCalendar();
 
+// 이번달, 다음달을 보여줄 nav 구현
 const prevMonth = () => {
   date.setMonth(date.getMonth() - 1);
-  renderCalender();
-};
+  renderCalendar();
+}
 
 const nextMonth = () => {
   date.setMonth(date.getMonth() + 1);
-  renderCalender();
-};
+  renderCalendar();
+}
 
 const goToday = () => {
   date = new Date();
-  renderCalender();
-};
+  renderCalendar();
+}
